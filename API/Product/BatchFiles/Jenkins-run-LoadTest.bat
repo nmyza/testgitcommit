677fd504f99729f703a0e0a7de9a70586143747b
narepal <narepal@cdw.com>
2018-03-09 15:42:28 -0600

    commenting additonal params

--- API/Product/BatchFiles/Jenkins-run-LoadTest.bat
+++ API/Product/BatchFiles/Jenkins-run-LoadTest.bat
@@ -4,21 +4,22 @@ set testPlanFile=%1
 set resultsFile=%2
 set resultsFileSummary=%3
 set baseUrl=%4
-set versionNumber = %5
-set rampup = %6
-set threads =%7
-set loop = %8
-set ProductCodesCSVFile = %9
-set BidContractReferenceNumbersCSVFile = %10
-set CanadaProductCodesCSVFile = %11
-set DiscontinuedProductCodesCSVFile = %12
-set ManufactureCodesCSVFile = %13
-set Top100DEVSearchProductCodesCSVFile = %14
-set ProductcodesWithReviewscsv = %15
+set versionNumber=%5
+set rampup=%6
+set threads=%7
+set loop=%8
+rem set ProductCodesCSVFile=%9
+rem set BidContractReferenceNumbersCSVFile=%10
+rem set CanadaProductCodesCSVFile=%11
+rem set DiscontinuedProductCodesCSVFile=%12
+rem set ManufactureCodesCSVFile=%13
+rem set Top100DEVSearchProductCodesCSVFile=%14
+rem set ProductcodesWithReviewscsv=%15
 
 
 del -f %resultsFile%
 del -f %resultsFileSummary%
 
-call jmeter.bat -JPROTOCOL=https -JBASE_URL=%baseUrl% -JVersionNumber=%versionNumber% -JRESULTS_FILE=%resultsFile% -JProductCodesCSVFile=%ProductcodesCsvFile% -JBidContractReferenceNumbersCSVFile=%BidContractReferenceNumbersCSVFile%  -JCanadaProductCodesCSVFile=%CanadaProductCodesCSVFile% -JDiscontinuedProductCodesCSVFile=%DiscontinuedProductCodesCSVFile% -JPRODUCTCODES_CSVFILE=%ProductcodesCsvFile% -JManufactureCodesCSVFile=%ManufactureCodesCSVFile% -JTop100DEVSearchProductCodesCSVFile=%Top100DEVSearchProductCodesCSVFile% -JProductcodesWithReviewscsv=%ProductcodesWithReviewscsv% -JRAMPUP=%rampup% -JTHREADS=%threads%  -JLOOP=%loop% -n -t %testPlanFile%
+rem call jmeter.bat -JPROTOCOL=https -JBASE_URL=%baseUrl% -JVersionNumber=%versionNumber% -JRESULTS_FILE=%resultsFile% -JProductCodesCSVFile=%ProductcodesCsvFile% -JBidContractReferenceNumbersCSVFile=%BidContractReferenceNumbersCSVFile%  -JCanadaProductCodesCSVFile=%CanadaProductCodesCSVFile% -JDiscontinuedProductCodesCSVFile=%DiscontinuedProductCodesCSVFile% -JPRODUCTCODES_CSVFILE=%ProductcodesCsvFile% -JManufactureCodesCSVFile=%ManufactureCodesCSVFile% -JTop100DEVSearchProductCodesCSVFile=%Top100DEVSearchProductCodesCSVFile% -JProductcodesWithReviewscsv=%ProductcodesWithReviewscsv% -JRAMPUP=%rampup% -JTHREADS=%threads%  -JLOOP=%loop% -n -t %testPlanFile%
+call jmeter.bat -JPROTOCOL=https -JBASE_URL=%baseUrl% -JVersionNumber=%versionNumber% -JRESULTS_FILE=%resultsFile% -JRAMPUP=%rampup% -JTHREADS=%threads%  -JLOOP=%loop% -n -t %testPlanFile%
 call JMeterPluginsCMD.bat --generate-csv %resultsFileSummary% --input-jtl %resultsFile% --plugin-type AggregateReport
