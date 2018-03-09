e6521b28d341f3c70b7380625717dd6a913ae9ee
narepal <narepal@cdw.com>
2018-03-09 16:15:39 -0600

    removing the source file name param

--- API/Price/BatchFiles/Jenkin-run-LoadTest.bat
+++ API/Price/BatchFiles/Jenkin-run-LoadTest.bat
@@ -5,14 +5,13 @@ set resultsFile=%2
 set resultsFileSummary=%3
 set baseUrl=%4
 set versionNumber = %5
-set ProductcodesCsvFile = %6
-set rampup = %7
-set threads =%8
-set loop = %9
+set rampup = %6
+set threads =%7
+set loop = %8
 
 
 del -f %resultsFile%
 del -f %resultsFileSummary%
 
-call jmeter.bat -JPROTOCOL=https -JBASE_URL=%baseUrl% -JVersionNumber=%versionNumber% -JRESULTS_FILE=%resultsFile% -JPRODUCTCODES_CSVFILE=%ProductcodesCsvFile% -JRAMPUP=%rampup% -JTHREADS=%threads%  -JLOOP=%loop% -n -t %testPlanFile%
+call jmeter.bat -JPROTOCOL=https -JBASE_URL=%baseUrl% -JVersionNumber=%versionNumber% -JRESULTS_FILE=%resultsFile% -JRAMPUP=%rampup% -JTHREADS=%threads%  -JLOOP=%loop% -n -t %testPlanFile%
 call JMeterPluginsCMD.bat --generate-csv %resultsFileSummary% --input-jtl %resultsFile% --plugin-type AggregateReport
